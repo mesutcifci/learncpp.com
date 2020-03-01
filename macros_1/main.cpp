33f82c98adcb5b893b3fc13dc19fdcb75ff99f5c
Mesut Çifci <mesutcifci97@gmail.com>
2020-03-01 16:04:10 +0300

    #define

---
+++ macros_1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#define NAME "MESUT" // All NAMEs are replaced 'MESUT' before the compilation phase.
+
+int main()
+{
+    std::cout << "Hello "<< NAME << "\n";
+    std::cout << "Him name is " << NAME << "\n";
+    return 0;
+}
