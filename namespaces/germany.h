d9d9a807f23f507e96bdae7183d3ec126bbae142
Mesut Çifci <mesutcifci97@gmail.com>
2020-03-10 16:43:46 +0300

    namespaces

---
+++ namespaces/germany.h
@@ -0,0 +1,9 @@
+#ifndef GERMANY_H
+#define GERMANY_H
+
+#include <iostream>
+namespace germany {
+    void sayHello();
+}
+
+#endif
---
+++ namespaces/japan.h
@@ -0,0 +1,12 @@
+#ifndef JAPAN_H
+#define JAPAN_H
+
+#include <iostream>
+namespace japan{
+    void sayHello();
+    namespace morning {
+        void sayMorning();
+    }
+}
+
+#endif
